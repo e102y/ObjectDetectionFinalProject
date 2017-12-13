fc8a1c2260b6305fbee8af2c793bde8ef0eaef17
Richarbe <student@students-iMac-198.local>
2017-12-12 23:28:08 -0800

    refactored some, commented some

--- main.m
+++ main.m
@@ -1,18 +1,14 @@
 scale = 1 ; %how is the image scaled?
 %sourceimage url is http://vision.middlebury.edu/stereo/submit3/zip/MiddEval3-data-F.zip
-num = 9 ; %4,5 are good; 6(good) versus 7(bad) shows lighting effect. 
+num = 4 ; %4 is good; 6(good) versus 7(bad) shows lighting effect. 
 numberString = int2str(num) ;
 
-pfx = fullfile('Images', 'trainingQ', numberString, {'im0.png', 'im1.png'}) ;
+pfx = fullfile('Images', 'testQ', numberString, {'im0.png', 'im1.png'}) ;
 
 image = imread(char(pfx(:,1))) ;
 image1 = imresize(image, scale) ;
 image = imread(char(pfx(:,2))) ;
 image2 = imresize(image, scale) ;
 
-matchedPoints = generateMatchedPoints(image1, image2);
-
-%%
-%should get MatchedPairs
-depthmap = generateDepthMap(image1, matchedPoints);
+depthmap = depthFromStereoImages(image1, image2);
 newimg  = rgb2hsDMv(image1, depthmap);
