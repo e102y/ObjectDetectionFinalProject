1ae007aae9e7cefb92e943499f46a844cb2c9e1a
Richarbe <student@students-iMac-198.local>
2017-12-12 18:40:50 -0800

    Fixed main and functions

--- main.m
+++ main.m
@@ -1,6 +1,13 @@
+scale = 0.3 ; %how is the image scaled?
+
 pfx = fullfile('Images', 'storage0.png') ; 
 image = imread(pfx) ;
-image = imresize(image, scale) ;
-matchedPoints = generateMatchedPoints(image);
+image1 = imresize(image, scale) ;
+
+pfx = fullfile('Images', 'storage1.png') ; 
+image = imread(pfx) ;
+image2 = imresize(image, scale) ;
+
+matchedPoints = generateMatchedPoints(image1, image2);
 %should get MatchedPairs
-generateDepthMap(image, matchedPoints);
+depthmap = generateDepthMap(image1, matchedPoints);
