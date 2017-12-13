e7f9ce917f0a67493f37024c4589c3fc60e6f4a0
Jeff Booher-Kaeding <jeffbk95@gmail.com>
2017-12-12 23:55:17 -0800

    Starting submission for sprint two

--- CODE/Depth map/main.m
+++ CODE/Depth map/main.m
@@ -2,7 +2,7 @@ clc
 close all
 scale = 1 ; %how is the image scaled?
 %sourceimage url is http://vision.middlebury.edu/stereo/submit3/zip/MiddEval3-data-F.zip
-num = 4 ; %4 is good; 6(good) versus 7(bad) shows lighting effect. 
+num = 6 ; %4 is good; 6(good) versus 7(bad) shows lighting effect. 
 numberString = int2str(num) ;
 
 pfx = fullfile('Images', 'testQ', numberString, {'im0.png', 'im1.png'}) ;
