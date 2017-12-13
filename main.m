df62b578e1a26db11098e9b9046f844895045ee0
Jeff Booher-Kaeding <jeffbk95@gmail.com>
2017-12-12 22:37:12 -0800

    fixing things again.

--- main.m
+++ main.m
@@ -2,11 +2,7 @@ clc
 close all
 scale = 1 ; %how is the image scaled?
 %sourceimage url is http://vision.middlebury.edu/stereo/submit3/zip/MiddEval3-data-F.zip
-<<<<<<< Updated upstream
 num = 9 ; %4,5 are good; 6(good) versus 7(bad) shows lighting effect. 
-=======
-num = 4 ;
->>>>>>> Stashed changes
 numberString = int2str(num) ;
 
 pfx = fullfile('Images', 'trainingQ', numberString, {'im0.png', 'im1.png'}) ;
