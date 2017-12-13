8c9d2c26e479fbcf0f923abfd1a825c5280a8e84
Richarbe <student@students-iMac-198.local>
2017-12-13 00:42:01 -0800

    2 more image sets

--- CODE/Depth map/main.m
+++ CODE/Depth map/main.m
@@ -2,10 +2,10 @@ clc
 close all
 scale = 1 ; %how is the image scaled?
 %sourceimage url is http://vision.middlebury.edu/stereo/submit3/zip/MiddEval3-data-F.zip
-num = 1 ; %4 is good; 6(good) versus 7(bad) shows lighting effect. 
+num = 4 ; %4 is good; 6(good) versus 7(bad) shows lighting effect. 
 numberString = int2str(num) ;
 
-pfx = fullfile('Images', 'testQ', numberString, {'im0.png', 'im1.png'}) ;
+pfx = fullfile('Images', 'trainingQ', numberString, {'im0.png', 'im1.png'}) ;
 
 image = imread(char(pfx(:,1))) ;
 image1 = imresize(image, scale) ;
